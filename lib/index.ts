e5dfd7ff7217db568121812b8aa1127fdfed3656
Mihail_Kozhevnikov <makozhevnikov@vtb.ru>
2020-03-22 06:46:10 +0300

    fix(configure-package): added types

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,4 @@
 export { RestRequest } from '@/requests/base-requests/rest-request';
 export { errorConstructor } from '@/errors/error-constructor';
+
+export { IBaseResponse, QueryParamsType } from '@/index.d.ts';
