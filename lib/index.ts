7c1ba440c92d2366d0670098f7ffc601d9a62f0a
Mihail_Kozhevnikov <makozhevnikov@vtb.ru>
2020-03-22 06:56:25 +0300

    fix(configure-package): added to export some utils

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 export { RestRequest } from '@/requests/base-requests/rest-request';
 export { errorConstructor } from '@/errors/error-constructor';
-
+export {
+  getFormattedResponseErrorText,
+} from '@/errors/get-formatted-response-error';
 export { IBaseResponse, QueryParamsType } from '@/index.d.ts';
