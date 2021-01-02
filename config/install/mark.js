45cb5caff3b32f1165bffdbf6827ab3ea33676c3
mihanizm56 <m727507.56@mail.ru>
2021-01-03 02:00:56 +0300

    build(configure-package): v3.5.1

--- config/install/mark.js
+++ config/install/mark.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const colors = require('colors');
 
 const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
 
 const main = async () => {
   console.log(
-    'If you like @mihanizm/fetch-api - please rate me in Github !!!!'.red.bold
+    'If you like @mihanizm56/fetch-api - please rate me in Github !!!!'.red.bold
       .underline,
   );
 
