bfcf5c951165560480b3bfffa32d86927bde94de
mihanizm56 <m727507.56@mail.ru>
2021-01-12 23:00:48 +0300

    update docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,7 +95,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('/docs/overview')}>
               Get Started
             </Link>
           </div>
